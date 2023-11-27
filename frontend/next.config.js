ed70addf4cd862d26cd259cbfc41599ebe67f068
Venkadesh <venkadeshshiva1@gmail.com>
2023-11-27 23:24:16 +0530

    clean up and fixed issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: "export",
+  
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+  
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+  
+    // Optional: Change the output directory `out` -> `dist`
+    distDir: "dist",
+    compress: false,
+  };
+  
+  module.exports = nextConfig;
+  
